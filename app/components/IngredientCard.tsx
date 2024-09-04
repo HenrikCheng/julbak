c6c440f0ae6b766897ae78b417de6a48f11b41ed
HenrikCheng <henrikcheng@live.se>
2024-09-04 23:32:59 +0200

    feat: update styles on cards

--- app/components/IngredientCard.tsx
+++ app/components/IngredientCard.tsx
@@ -18,43 +18,46 @@ const IngredientCard = ({
 	const [open, setOpen] = useState(false);
 
 	return (
-		<li className="border border-solid border-gray-500 rounded-md my-4">
-			<div className="p-3 mb-4">
-				<h2>{ingredient.name}</h2>
-				<ul>
+		<>
+			<li className="m-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+				<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					{ingredient.name}
+				</h5>
+				<div className="font-normal text-gray-700 dark:text-gray-400">
 					<li>
 						Desired amount: {ingredient.desired_amount} {ingredient.unit}
 					</li>
 					<li>
 						Current amount: {ingredient.current_amount} {ingredient.unit}
 					</li>
-				</ul>
-				<Button color="blue" onClick={() => setOpen(!open)}>
-					Jag kan bidra
-				</Button>
-				{open && (
-					<Form
-						setIngredients={setIngredients}
-						setError={setError}
-						ingredientId={ingredient._id}
-						setOpen={setOpen}
-					/>
-				)}
-			</div>
-			{ingredient.contributors?.length > 0 &&
-				ingredient.contributors.map(
-					(contributor: Contributor, index: number) => (
-						<IngredientPost
+					<Button color="blue" onClick={() => setOpen(!open)}>
+						{open ? "Stäng" : "Jag kan bidra"}
+					</Button>
+					{open && (
+						<Form
 							setIngredients={setIngredients}
 							setError={setError}
-							contributor={contributor}
-							index={index}
-							ingredient={ingredient}
-							key={contributor.contributor}
+							ingredientId={ingredient._id}
+							setOpen={setOpen}
 						/>
-					),
-				)}
-		</li>
+					)}
+				</div>
+
+				{ingredient.contributors?.length > 0 &&
+					ingredient.contributors.map(
+						(contributor: Contributor, index: number) => (
+							<IngredientPost
+								setIngredients={setIngredients}
+								setError={setError}
+								contributor={contributor}
+								index={index}
+								ingredient={ingredient}
+								key={contributor.contributor}
+							/>
+						),
+					)}
+			</li>
+		</>
 	);
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Jumbotron from "./components/Jumbotron";
 import IngredientList from "./components/IngredientList";
+import Footer from "./components/Footer";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-start">
 			<Jumbotron />
 			<IngredientList />
+			<Footer />
 		</main>
 	);
 }
