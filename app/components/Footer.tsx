daddc71a74bf8767a2a7dc021007d238e753eb16
HenrikCheng <henrikcheng@live.se>
2024-09-13 14:53:36 +0200

    feat: footer design upgrade

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
 	return (
 		<footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 max-w-screen-xl w-full">
-			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+			<div className="w-full mx-auto max-w-screen-xl py-4 px-8 gap-2 flex-col-reverse md:flex-row flex items-center justify-between">
 				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
 					© 2024{" "}
 					<Link
