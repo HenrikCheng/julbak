8ca49be4952d5f853f465f18f75cc200a18579e2
HenrikCheng <henrikcheng@live.se>
2024-09-20 15:32:07 +0200

    refactor: timslotday code

--- app/components/TimeSlotCalendar/TimeSlotDay.tsx
+++ app/components/TimeSlotCalendar/TimeSlotDay.tsx
@@ -1,5 +1,5 @@
 import { TimeSlot, TimeSlotAPI } from "@/app/types";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import Button from "../Button";
 import { Session } from "next-auth";
 import loginFeature from "@/app/config/featureFlags";
@@ -18,51 +18,41 @@ const TimeSlotDay = ({
 	setCalendar,
 	session,
 }: TimeSlotDayProps) => {
-	const toggleAdmission = async ({ startTime, slot }: any) => {
+	const toggleAdmission = async ({
+		startTime,
+		slot,
+	}: {
+		startTime: string;
+		slot: string;
+	}) => {
 		try {
-			// Check if the entry already exists
 			const existingEntry = calendar.find(
 				(entry) => entry.date === startTime && entry.position === slot,
 			);
 
-			if (existingEntry) {
-				// Clear the existing entry
-				const response = await fetch(`/api/calendar?_id=${existingEntry._id}`, {
-					method: "DELETE",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				});
+			const response = await fetch(
+				`/api/calendar${existingEntry ? `?_id=${existingEntry._id}` : ""}`,
+				{
+					method: existingEntry ? "DELETE" : "POST",
+					headers: { "Content-Type": "application/json" },
+					body: existingEntry
+						? undefined
+						: JSON.stringify({
+								name: session?.user?.name || "Anonymous",
+								date: startTime,
+								position: slot,
+						  }),
+				},
+			);
 
+			if (!response.ok) {
 				const result = await response.json();
-				if (!response.ok) {
-					throw new Error(result.error || "Something went wrong");
-				}
-			} else {
-				// Add new entry
-				const response = await fetch("/api/calendar", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						name: session?.user?.name || "Anonymous", // Use session user name or fallback to 'Anonymous'
-						date: startTime,
-						position: slot,
-					}),
-				});
-
-				const result = await response.json();
-				if (!response.ok) {
-					throw new Error(result.error || "Something went wrong");
-				}
+				throw new Error(result.error || "Something went wrong");
 			}
 
-			// Re-fetch updated calendar data
 			const updatedCalendarResponse = await fetch("/api/calendar");
-			const updatedCalendar = await updatedCalendarResponse.json();
 			if (updatedCalendarResponse.ok) {
-				setCalendar(updatedCalendar);
+				setCalendar(await updatedCalendarResponse.json());
 			} else {
 				throw new Error("Failed to re-fetch updated calendar data");
 			}
@@ -71,79 +61,53 @@ const TimeSlotDay = ({
 		}
 	};
 
+	const renderButton = (slot: TimeSlot, slotNumber: string) => {
+		const isRegistered = calendar.some(
+			(entry) => entry.date === slot.startTime && entry.position === slotNumber,
+		);
+		const buttonText = isRegistered
+			? session?.user?.name || session?.user?.email || ""
+			: "Anmäl mig";
+
+		return (
+			<Button
+				color={isRegistered ? undefined : "gray"}
+				onClick={() =>
+					toggleAdmission({ startTime: slot.startTime, slot: slotNumber })
+				}
+			>
+				{buttonText}
+			</Button>
+		);
+	};
+
 	return (
 		<div className="grid grid-cols-1 gap-2">
 			{weekday.map((slot) => (
 				<div
 					key={slot.startTime}
-					className="grid gap-4  border-t-2 py-4 border-gray-500"
+					className="grid gap-4 border-t-2 py-4 border-gray-500"
 				>
 					<div className="flex flex-col gap-1">
 						<span className="font-medium">{slot.duration}</span>
 						<span className="text-sm text-muted-foreground">{slot.label}</span>
 					</div>
+
 					{loginFeature && session ? (
 						<div className="flex items-center justify-end gap-2">
-							{calendar.find(
-								(entry) =>
-									entry.date === slot.startTime && entry.position === "1",
-							) ? (
-								<Button
-									color="green"
-									onClick={() =>
-										toggleAdmission({ startTime: slot.startTime, slot: "1" })
-									}
-								>
-									{session?.user?.name || session?.user?.email || ""}
-								</Button>
-							) : (
-								<Button
-									color="gray"
-									onClick={() =>
-										toggleAdmission({ startTime: slot.startTime, slot: "1" })
-									}
-								>
-									Anmäl mig
-								</Button>
-							)}
-
-							{calendar.find(
-								(entry) =>
-									entry.date === slot.startTime && entry.position === "2",
-							) ? (
-								<Button
-									color="green"
-									onClick={() =>
-										toggleAdmission({ startTime: slot.startTime, slot: "2" })
-									}
-								>
-									{session?.user?.name || session?.user?.email || ""}
-								</Button>
-							) : (
-								<Button
-									color="gray"
-									onClick={() =>
-										toggleAdmission({ startTime: slot.startTime, slot: "2" })
-									}
-								>
-									Anmäl mig
-								</Button>
-							)}
+							{["1", "2"].map((slotNumber) => renderButton(slot, slotNumber))}
 						</div>
 					) : (
 						<form className="flex items-center justify-end gap-2">
-							<TimeSlotField
-								startTime={slot.startTime}
-								calendar={calendar}
-								slot="1"
-								setCalendar={setCalendar}
-							/>
-							<TimeSlotField
-								startTime={slot.startTime}
-								calendar={calendar}
-								slot="2"
-								setCalendar={setCalendar}
-							/>
+							{["1", "2"].map((slotNumber) => (
+								<TimeSlotField
+									key={slotNumber}
+									startTime={slot.startTime}
+									calendar={calendar}
+									slot={slotNumber}
+									setCalendar={setCalendar}
+								/>
+							))}
 						</form>
 					)}
 				</div>
