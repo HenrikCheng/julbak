0f61d212ea5a3bbcddff39e87cfdf54e5715b4ad
HenrikCheng <henrikcheng@live.se>
2024-10-24 08:31:53 +0200

    fix: remove navbar to fix build issue

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import React from "react";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
+import { SessionProvider } from "next-auth/react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-between align-middle">
-			<Navbar />
-			<main>{children}</main>
-			<Image
-				src="/gingerbread.svg"
-				alt="Gingerbread Man"
-				width={150}
-				height={150}
-				className="self-center justify-center text-center"
-			/>
-			<Footer />
-		</div>
+		<SessionProvider>
+			<div className="flex min-h-screen flex-col items-center justify-between align-middle">
+				{/* <Navbar /> */}
+				<main>{children}</main>
+				<Image
+					src="/gingerbread.svg"
+					alt="Gingerbread Man"
+					width={150}
+					height={150}
+					className="self-center justify-center text-center"
+				/>
+				<Footer />
+			</div>
+		</SessionProvider>
 	);
 };
 
